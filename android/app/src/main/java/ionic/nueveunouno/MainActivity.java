62b6d28d02db951e5eb625d82542ea04bd615408
DarkWister <darkwistercrak17@gmail.com>
2025-03-18 14:13:44 +0000

    init

---
+++ android/app/src/main/java/ionic/nueveunouno/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.nueveunouno;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.nueveunouno',
+  appName: 'nueveunouno',
+  webDir: 'www'
+};
+
+export default config;
