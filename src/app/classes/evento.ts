73e8fb8639871011344369aff21ffd7e5b5fe52e
DarkWister <darkwistercrak17@gmail.com>
2025-03-18 14:30:06 +0000

    classe init

---
+++ src/app/classes/evento.ts
@@ -0,0 +1,9 @@
+export class Evento {
+    constructor(
+        public id?: number,
+        private titulo?: string,
+        private descripcion?: string,
+        private fecha?: string,
+        private foto?: string
+    ){}
+}
